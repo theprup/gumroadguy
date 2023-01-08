2ae68c4bb0f13e76ca10e0bd0bddde2568d5bf42
theprup <62210209+theprup@users.noreply.github.com>
2023-01-08 18:46:13 +0100

    commit all

--- slices/TextSlice/index.js
+++ slices/TextSlice/index.js
@@ -1,35 +1,49 @@
-import React from 'react'
-import { PrismicRichText, PrismicLink} from '@prismicio/react'
-import {  } from '@prismicio/react'
+import React from "react";
+import { PrismicRichText, PrismicLink } from "@prismicio/react";
+import {} from "@prismicio/react";
+import { asDate } from "@prismicio/helpers";
+
 
 const TextSlice = ({ slice }) => (
   <section>
-  <PrismicRichText field={slice.primary.eyebrow} />
-    <span className="title">
-      {
-        slice.primary.title ?
-        <PrismicRichText field={slice.primary.title}/>
-        : <h2>Template slice, update me!</h2>
-      }
-    </span>
-   
-      <PrismicRichText field={slice.primary.paragraph} />
-      
+    <div className="eyebrow">
+      <PrismicRichText field={slice.primary.eyebrow} />
+    </div>
+    <div >
+      {slice.primary.title ? (
+        <PrismicRichText field={slice.primary.title} components={{
+          heading1: ({ children }) => <h1 className="title">{children}</h1>
+        }
+        }/>
+      ) : (
+        <h2>Template slice, update me!</h2>
+      )}
+    </div>
 
+    <PrismicRichText field={slice.primary.paragraph} />
 
-    <PrismicLink  field={slice.primary.link}>{ slice.primary.label }</PrismicLink>
+    <PrismicLink field={slice.primary.link}>{slice.primary.label}</PrismicLink>
 
     <style jsx>{`
-        section {
-          max-width: 600px;
-          margin: 4em auto;
-          text-align: center;
-        }
-        .title {
-          color: #8592e0;
-        }
+      section {
+        text-align: center;
+        background-color: #ff90e8;
+        font-family: 'Mabry pro', sans-serif;
+        padding: 2em;
+      }
+      .title {
+        font-size: 5rem;
+        padding-right: 150px;
+        padding-left: 150px;
+        font-weight: lighter;
+        margin-block-start: 0em;
+        margin-block-end: 0em;
+      }
+      .eyebrow {
+        font-weight: bold;
+      }
     `}</style>
   </section>
-)
+);
 
-export default TextSlice
+export default TextSlice;
